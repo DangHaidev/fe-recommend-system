9ec8dc080de33637db8021edca407c8647bfff6f
Hai Dang <dangd408@gmail.com>
2025-11-25 22:55:16 +0700

    feat: search api - doing get review movie by api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org', // Example: Unsplash images
+            },
+
+            // Add more objects for other allowed domains
+        ],
+    },
 };
 
 export default nextConfig;
