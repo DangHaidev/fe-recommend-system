b68ff5d85b163266acabd98a8981901c9e7e7bed
Hai Dang <dangd408@gmail.com>
2025-12-06 23:21:55 +0700

    refactor header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,10 +4,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 import SearchBar from '../common/SearchBar';
+import { useSession } from 'next-auth/react';
+import { h1 } from 'framer-motion/client';
 
 export function Header() {
     const [menuOpen, setMenuOpen] = useState(false); // mobile menu
     const [openMenu, setOpenMenu] = useState<string | null>(null); // dropdown
+    const { data: session } = useSession();
 
     const toggleDropdown = (menu: string) => {
         setOpenMenu(openMenu === menu ? null : menu);
@@ -47,79 +50,22 @@ export function Header() {
                             <ul className="header__nav">
                                 {/* Home */}
                                 <li className="header__nav-item">
-                                    <button
+                                    <Link
+                                        href={'/home'}
                                         className="header__nav-link"
-                                        type="button"
-                                        onClick={() => toggleDropdown('home')}
                                     >
                                         Home
-                                    </button>
-                                    <ul
-                                        className={`dropdown-menu header__nav-menu ${
-                                            openMenu === 'home' ? 'show' : ''
-                                        }`}
-                                    >
-                                        <li>
-                                            <Link href="/">Home style 1</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/index2">
-                                                Home style 2
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/index3">
-                                                Home style 3
-                                            </Link>
-                                        </li>
-                                    </ul>
+                                    </Link>
                                 </li>
 
                                 {/* Catalog */}
                                 <li className="header__nav-item">
-                                    <button
+                                    <Link
                                         className="header__nav-link"
-                                        type="button"
-                                        onClick={() =>
-                                            toggleDropdown('catalog')
-                                        }
+                                        href={'/category'}
                                     >
                                         Catalog
-                                    </button>
-                                    <ul
-                                        className={`dropdown-menu header__nav-menu ${
-                                            openMenu === 'catalog' ? 'show' : ''
-                                        }`}
-                                    >
-                                        <li>
-                                            <Link href="/catalog">Catalog</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/category">
-                                                Category style 1
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/category2">
-                                                Category style 2
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/details">
-                                                Details style 1
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/details2">
-                                                Details style 2
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/details3">
-                                                Details style 3
-                                            </Link>
-                                        </li>
-                                    </ul>
+                                    </Link>
                                 </li>
 
                                 {/* Pricing */}
@@ -216,15 +162,20 @@ export function Header() {
                                     </svg>
                                 </button>
 
-                                <Link href="/signin" className="header__user">
-                                    <span>Sign in</span>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
+                                {session?.user?.name ?? (
+                                    <Link
+                                        href="/signin"
+                                        className="header__user"
                                     >
-                                        <path d="M20,12a1,1,0,0,0-1-1H11.41l2.3-2.29a1,1,0,1,0-1.42-1.42l-4,4a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l4,4a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L11.41,13H19A1,1,0,0,0,20,12ZM17,2H7A3,3,0,0,0,4,5V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V16a1,1,0,0,0-2,0v3a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V8a1,1,0,0,0,2,0V5A3,3,0,0,0,17,2Z" />
-                                    </svg>
-                                </Link>
+                                        <span>Sign in</span>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M20,12a1,1,0,0,0-1-1H11.41l2.3-2.29a1,1,0,1,0-1.42-1.42l-4,4a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l4,4a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L11.41,13H19A1,1,0,0,0,20,12ZM17,2H7A3,3,0,0,0,4,5V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V16a1,1,0,0,0-2,0v3a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V8a1,1,0,0,0,2,0V5A3,3,0,0,0,17,2Z" />
+                                        </svg>
+                                    </Link>
+                                )}
                             </div>
                         </div>
                     </div>
